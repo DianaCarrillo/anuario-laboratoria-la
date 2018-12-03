1c145abf786ce17e339d61eae11d091bff575664
DianaCarrillo <39560661+DianaCarrillo@users.noreply.github.com>
2018-12-03 17:50:46 -0600

    logo y boton

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import logo from '../img/black-logo.png'
 
 const styles = {
 button: {
     backgroundColor : '#4267b2',
     color : 'white',
-    height: '50px'
+    height: '50px',
+    display: 'block'
 },
 
 container: {
@@ -15,14 +17,28 @@ container: {
     alignContent: 'center'
 },
 
-fbicon: {
+fbIcon: {
     marginRight: '15px'
+},
+
+img: {
+    display: 'block'
 }
+
 
 }
 export default () =>(
 <div className = "container" style = {styles.container}>
-    <button type="button" className = "btn col-lg-3 " style = {styles.button}><i className="fab fa-facebook-square" style ={styles.fbicon}></i> Acceder con Facebook</button>
+    <div className="row">
+        <div className="col-12">
+            <img src = {logo} height = "10" alt="" style={styles.img}/>
+        </div>
+    </div>
+    <div className="row">
+        <div className="col-12">
+            <button type="button" className = "btn col-lg-3 " style = {styles.button}><i className="fab fa-facebook-square" style ={styles.fbIcon}></i> Acceder con Facebook</button>
+        </div>
+    </div>
 </div>
 
 )
