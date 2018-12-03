ef509d67d7b05217e13e85f86d3e3600a1409670
Lupo Montero <lupomontero@gmail.com>
2018-12-03 14:05:27 -0500

    agrega boilerplate de fetchData y fetchData.spec

---
+++ src/fetchData.js
@@ -0,0 +1,7 @@
+export const fetchPostsFromInstagram = hashtag => new Promise((resolve, reject) => {
+  // comprobar que hashtag es un string no vacío
+  // llamar API de Instagram
+  resolve([]);
+});
+
+// const fetchPosts = hashtag => Promise.resolve({});
